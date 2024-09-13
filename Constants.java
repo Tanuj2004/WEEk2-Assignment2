06424e07df527104cf9610332ab25c12beadc8d9
Tanuj2004 <22egjcs811@gitjaipur.com>
2024-09-13 09:54:56 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,22 @@
+/**
+ * This class contains a set of constant strings used throughout the program
+ * to display messages and show the flow of operations on strings.
+ * Owner: Tanuj Yadav
+ * Date: 11/09/2024
+ */
+public class Constants {
+    public static final String INVALID_INPUT_MESSAGE = "Invalid input. Please enter a number.";
+    public static final String INVALID_NUMBER = "Invalid number format.";
+    public static final String INVALID_POSITION = "Invalid Fibonacci position.";
+    public static final String INVALID_OPTION = "Invalid choice. Enter a number between 1 and 6.";
+    public static final String TASK_NUMBER = "Enter a Task Number to perform between 1 and 6 only";
+    public static final String BINARY_STRING_PROMPT = "Enter a binary string:";
+    public static final String POSITION_PROMPT = "Enter the position for Fibonacci:";
+    public static final String STRING_PROMPT = "Enter a string:";
+    public static final String NTH_NUMBER = "Nth Fibonacci number is: ";
+    public static final String CAMELCASE = "Converted to camelCase: ";
+    public static final String CONSONANT = "Number of consonants: ";
+    public static final String PALINDROME_COUNT_PROMPT = "Total unique palindromic substrings: ";
+    public static final String PALINDROME_STRING = "Total unique palindromic substrings: ";
+    public static final String INDEX = "Choose a task: \n 1: Count Palindromes \n 2: Find Nth Fibonacci Number \n 3: Convert Snake Case to Camel Case \n 4: Count Consonants \n 5: Convert Binary to Decimal \n 6: Exit";
+}
